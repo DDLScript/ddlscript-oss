eba56b1fb3f558daef6dbe72aa2ca422ce074289
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-05-19 16:53:45 +1000

    move function into a dedicated Resource Utility class

--- src/main/java/com/ddlscript/repository/embedded/EmbeddedDatabaseMutator.java
+++ src/main/java/com/ddlscript/repository/embedded/EmbeddedDatabaseMutator.java
@@ -1,16 +1,12 @@
 package com.ddlscript.repository.embedded;
 
-import lombok.NonNull;
+import com.ddlscript.utils.ResourceUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ColumnListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import javax.sql.DataSource;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -80,58 +76,20 @@ public class EmbeddedDatabaseMutator {
 				.orElseThrow();
 	}
 
-	/**
-	 * Reads in a given resource and returns it as a string.
-	 *
-	 * @param resourcePath
-	 * 		the uri path to the requested resource
-	 * @return The resource as a string
-	 */
-	private static String getResourceAsString(final String resourcePath) {
-		try (InputStream inputStream = EmbeddedDatabaseMutator.class.getResourceAsStream(resourcePath)) {
-			// Ensure the file exists.
-			if (inputStream == null) {
-				String errMessage = "Unable to read resource `" + resourcePath + "`.";
-
-				if (!resourcePath.startsWith("/")
-						&& EmbeddedDatabaseMutator.class.getResource("/" + resourcePath) != null) {
-					errMessage += " Were you meaning to include a slash at the beginning of the path (`" + "/"
-							+ resourcePath + "`)?";
-				}
-
-				throw new NullPointerException(errMessage);
-			}
-
-			// compile into a string
-			StringBuilder resultStringBuilder = new StringBuilder();
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-				String line;
-				while ((line = br.readLine()) != null) {
-					resultStringBuilder.append(line)
-							.append("\n");
-				}
-			}
-			return resultStringBuilder.toString();
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
-
 	private static Consumer<File> getMutationApplicator(final QueryRunner withQueryRunner) {
 		return withFile -> {
 			final int mutationId = Integer.parseInt(withFile.getName()
 					.split("\\.")[0]);
 			LOGGER.info("Applying Mutation ID: " + mutationId);
 
-			final String sql = EmbeddedDatabaseMutator.getResourceAsString("/" + DDL_RESOURCE_FOLDER + withFile.getName());
+			final String sql = ResourceUtils.getResourceAsString("/" + DDL_RESOURCE_FOLDER + withFile.getName());
 
 			try {
 				withQueryRunner.execute(sql);
-				withQueryRunner.execute(
-						"INSERT INTO `DDLMigration` (VERSION, DDL, TIMESTAMP_APPLIED) VALUES (?, ?, CURRENT_TIMESTAMP"
-								+ "());",
-						mutationId, sql
-				);
+
+				final String insertSql = "INSERT INTO `DDLMigration` (VERSION, DDL, TIMESTAMP_APPLIED)"
+						+ " VALUES (?, ?, CURRENT_TIMESTAMP());";
+				withQueryRunner.execute(insertSql, mutationId, sql);
 			} catch (SQLException sqlException) {
 				throw new RuntimeException(sqlException);
 			}
