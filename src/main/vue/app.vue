cb7ed9ddf0e0950aca39c86d8abd64581f84ff82
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-06-20 11:46:30 +1000

    update the footer copywrite

--- src/main/vue/app.vue
+++ src/main/vue/app.vue
@@ -2,7 +2,7 @@
 	<ddlscript-element-globalheader />
 	<router-view />
 	<div style='padding:2rem 1rem 4rem;opacity:0.666;text-align:center;'>
-		<small>Copyright &copy; 2017-{{ new Date().getFullYear() }} DDL Script.<br />DDL Script is open sounce and can be found on <a href='https://github.com/ddlscript/ddlscript-oss' target='_blank' style='text-decoration:underline;'>GitHub</a></small>
+		<small>Copyright &copy; 2017-{{ new Date().getFullYear() }} DDL Script.<br />DDL Script is open source and can be forked on <a href='https://github.com/ddlscript/ddlscript-oss' target='_blank' style='text-decoration:underline;'>GitHub</a></small>
 	</div>
 </template>
 
