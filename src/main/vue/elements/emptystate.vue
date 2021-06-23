cd6c8c5d13d27eb255631bab135f55867c211fde
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-06-23 20:12:34 +1000

    adding an empty state component

---
+++ src/main/vue/elements/emptystate.vue
@@ -0,0 +1,22 @@
+<template>
+	<ddlscript-layout-center class="ddlscript-element-emptystate">
+		<span class="ddlscript-element-emptystate-title">{{ title }}</span>
+		<div class="ddlscript-element-emptystate-content"><slot name="default"></slot></div>
+	</ddlscript-layout-center>
+</template>
+
+<script>
+import CenterLayout from "../layouts/center.vue";
+
+export default {
+	name: "ddlscript-element-emptystate",
+
+	components: {
+		'ddlscript-layout-center': CenterLayout
+	},
+
+	props: {
+		title: { type:String, default:null }
+	}
+}
+</script>
---
+++ src/main/vue/layouts/center.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class='ddlscript-layout-center'><slot name="default"></slot></div>
+</template>
+
+<script>
+export default {
+	name: "ddlscript-layout-center"
+}
+</script>
