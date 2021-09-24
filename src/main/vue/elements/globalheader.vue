1bd0f7906944f7cdecd44a7318fdca71a2d0ea9a
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-09-24 14:04:42 +1000

    updates the global header bar to incluse a drop-down arrow and a hover indicator

--- src/main/vue/elements/globalheader.vue
+++ src/main/vue/elements/globalheader.vue
@@ -11,7 +11,10 @@
 		</div>
 
 		<!-- User -->
-		<span class='account' style="display:block;padding:0.5rem 1rem;margin-right:1rem;cursor:pointer;">{{ $session.created_by.name }}</span>
+		<div class='account'>
+			<span>{{ $session.created_by.name }}</span>
+			<svg-arrow-down />
+		</div>
 
 		<ddlscript-element-popover target=".ddlscript-element-globalheader > .account" trigger="click" position="bottom-end">
 			<ddlscript-element-panel :title="$session.created_by.username" hue="foreground" elevation="above" style="min-width:16rem;">
@@ -23,6 +26,7 @@
 
 <script>
 import LogoSvg from "../svgs/logo.vue";
+import ArrowDownSvg from "../svgs/arrowdown.vue";
 import PopOverElement from "../elements/popover.vue";
 import PanelElement from "../elements/panel.vue"
 import AccordionMenuItem from "../elements/accordionmenu/item.vue";
@@ -35,6 +39,7 @@ export default {
 
 	components: {
 		'svg-logo': LogoSvg,
+		'svg-arrow-down': ArrowDownSvg,
 		'ddlscript-element-popover': PopOverElement,
 		'ddlscript-element-panel': PanelElement,
 		'ddlscript-element-accordionmenu-item': AccordionMenuItem,
