b0558b7a416873e1fa570a0f9daf3bde94bf4260
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-11-02 16:00:05 +1100

    adds a layout wrapper to the settings page

--- src/main/vue/pages/projects/settings.vue
+++ src/main/vue/pages/projects/settings.vue
@@ -1,33 +1,37 @@
 <template>
-	<ddlscript-element-panel v-if="canEditSettings" hue="foreground" title="Settings">
-		<template v-slot:actions>
-			<ddlscript-element-button label="Save Changes" hue="positive" :disabled="!canSubmit" @click="onSaveClicked" />
-		</template>
+	<ddlscript-layout-center size="normal" style="padding-top:2rem;">
+		<ddlscript-element-panel v-if="canEditSettings" hue="foreground" title="Settings">
+			<template v-slot:actions>
+				<ddlscript-element-button label="Save Changes" hue="positive" :disabled="!canSubmit" @click="onSaveClicked" />
+			</template>
 
-		<div v-if="messages.error" data-hue="negative" style="text-align:center; padding: 1rem;">
-			{{ messages.error }}
-		</div>
+			<div v-if="messages.error" data-hue="negative" style="text-align:center; padding: 1rem;">
+				{{ messages.error }}
+			</div>
 
-		<div style="padding:0 1rem 1rem">
-			<!-- field: project name -->
-			<ddlscript-element-formfield title="Project Name" for="project_name" >
-				<ddlscript-element-input-textfield type='text' id='project_name' :value="updated_project.name" :disabled="is.submitting" maxlength="100" placeholder="... up to 100 characters ..." @updatevalue="updated_project.name = $event" @onkeyup="onProjectTitleKeyUp" />
-			</ddlscript-element-formfield>
-		</div>
+			<div style="padding:0 1rem 1rem">
+				<!-- field: project name -->
+				<ddlscript-element-formfield title="Project Name" for="project_name" >
+					<ddlscript-element-input-textfield type='text' id='project_name' :value="updated_project.name" :disabled="is.submitting" maxlength="100" placeholder="... up to 100 characters ..." @updatevalue="updated_project.name = $event" @onkeyup="onProjectTitleKeyUp" />
+				</ddlscript-element-formfield>
+			</div>
 
-	</ddlscript-element-panel>
+		</ddlscript-element-panel>
 
-	<ddlscript-element-panel v-if="canDelete" hue="foreground" title="Delete Project" style="margin-top:1rem;">
-		<template v-slot:actions>
-			<ddlscript-element-button label="Delete Project" hue="negative" :disabled="is.deleting" @click="onDeleteClicked" />
-		</template>
+		<ddlscript-element-panel v-if="canDelete" hue="foreground" title="Delete Project" style="margin-top:1rem;">
+			<template v-slot:actions>
+				<ddlscript-element-button label="Delete Project" hue="negative" :disabled="is.deleting" @click="onDeleteClicked" />
+			</template>
 
-		<p data-hue="negative" style="padding:1rem">This action will permanently delete the project and all it's related data. This action is permanent cannot be reveresed.</p>
-	</ddlscript-element-panel>
+			<p data-hue="negative" style="padding:1rem">This action will permanently delete the project and all it's related data. This action is permanent cannot be reveresed.</p>
+		</ddlscript-element-panel>
+	</ddlscript-layout-center>
 </template>
 
 <script>
 import DDLScript from "../../api";
+
+import CenterLayout from "../../layouts/center.vue";
 
 import PanelElement from "../../elements/panel.vue";
 import ButtonElement from "../../elements/button.vue";
@@ -38,6 +42,7 @@ export default {
 	name: 'ddlscript-page-project-settings',
 
 	components: {
+		'ddlscript-layout-center': CenterLayout,
 		'ddlscript-element-panel': PanelElement,
 		'ddlscript-element-button': ButtonElement,
 		'ddlscript-element-formfield': FormFieldElement,
